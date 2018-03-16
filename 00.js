0c860c6c8b39b955f29831e4350c9dedea73edbe
e <edge33w@gmail.com>
2018-03-16 16:18:11 +0800

    6补充内容

--- 00.js
+++ 00.js
@@ -59,3 +59,35 @@ doSomething().then(function () {
 doSomething().then(doSomethingElse());
 
 doSomething().then(doSomethingElse);
+
+
+
+function Person(first, last, age, gender, interests) {
+  this.name = {
+    first,
+    last
+  };
+  this.age = age;
+  this.gender = gender;
+  this.interests = interests;
+};
+
+Person.prototype.greeting = function() {
+  alert('Hi! I\'m ' + this.name.first + '.');
+};
+
+Teacher.prototype = Object.create(Person.prototype);
+
+Teacher.prototype.greeting = function() {
+  var prefix;
+
+  if(this.gender === 'male' || this.gender === 'Male' || this.gender === 'm' || this.gender === 'M') {
+    prefix = 'Mr.';
+  } else if(this.gender === 'female' || this.gender === 'Female' || this.gender === 'f' || this.gender === 'F') {
+    prefix = 'Mrs.';
+  } else {
+    prefix = 'Mx.';
+  }
+
+  alert('Hello. My name is ' + prefix + ' ' + this.name.last + ', and I teach ' + this.subject + '.');
+};
